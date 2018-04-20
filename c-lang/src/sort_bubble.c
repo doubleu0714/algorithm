0bfb5afc3a0da5107c7aa8081bd7c1545f9786e3
Wonhyung <park.wh14@gmail.com>
2018-04-20 22:55:57 +0900

    sudo code 추가

--- c-lang/src/sort_bubble.c
+++ c-lang/src/sort_bubble.c
@@ -2,8 +2,18 @@
  * Bubble sort 정렬 알고리즘
  * 원소가 N개인 배열 data[N]이 있다면 data[n] > data[n+1] 인 경우 두 자리를 바꾸어 주는 로직을 N-1 번 수행하면 최대 값이 data[N]에 존재하게된다.
  * 이후 data[N - 1] 배열을 대상으로 진행해 위의 로직을 N - 1번 반복한다.
+ * 
+ * 
  */
 #include <stdio.h>
+
+// bubblesort(A[], n)
+// {
+//     for last <- n down to 2 {
+//         for i <- 1 to last - 1
+//             if (A[i] > A[i + 1]) then A[i] <-> A[i + 1];
+//     }
+// }
 
 int main()
 {
@@ -13,7 +23,7 @@ int main()
 
     while(count > 1)
     {
-        // bubble change N - 1
+        // bubble change until N - 1
         while(idx < count - 1)
         {
             // data[n] > data[n + 1] 인 경우
@@ -23,6 +33,7 @@ int main()
                 int tmp = data[idx + 1];
                 data[idx + 1] = data[idx];
                 data[idx] = tmp;
+
                 for(int i = 0 ; i < sizeof(data) / sizeof(int) ; i++)
                 {
                     printf("%d ", data[i]);
@@ -31,6 +42,7 @@ int main()
             }
             idx++;
         }
+
         count--;
         idx = 0;
     }
